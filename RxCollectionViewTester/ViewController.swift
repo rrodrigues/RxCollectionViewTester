4cfb3539a4d22298d7e4f5e58ac928e740f9f52f
rui.rodrigues <rrodrigues99@gmail.com>
2019-01-15 23:12:54 +0000

    UI changes
    Add check/uncheck button
    Adjust cell layout
    Add show/remove all hidden buttons

--- RxCollectionViewTester/ViewController.swift
+++ RxCollectionViewTester/ViewController.swift
@@ -11,15 +11,30 @@ import SnapKit
 import RxSwift
 import RxCocoa
 
+extension UIEdgeInsets {
+    static func all(_ value: CGFloat) -> UIEdgeInsets {
+        return UIEdgeInsets(top: value, left: value, bottom: value, right: value)
+    }
+    static func symmetric(vertical: CGFloat, horizontal: CGFloat) -> UIEdgeInsets {
+        return UIEdgeInsets(top: vertical, left: horizontal, bottom: vertical, right: horizontal)
+    }
+}
+
+extension UIButton {
+    class func withTitle(_ title: String) -> Self {
+        let button = self.init(type: .system)
+        button.setTitle(title, for: .normal)
+        return button
+    }
+}
+
 class ViewController: UIViewController {
     
     let bag = DisposeBag()
     
-    let generateButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("Generate", for: .normal)
-        return button
-    }()
+    let generateButton = UIButton.withTitle("Generate")
+    let showAllButton = UIButton.withTitle("Show All")
+    let removeAllButton = UIButton.withTitle("Remove All")
     
     let collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
@@ -35,15 +50,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.addSubview(generateButton)
-        collectionView.contentInset = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        showAllButton.isHidden = true
+        removeAllButton.isHidden = true
+        let stackButtons = UIStackView(arrangedSubviews: [
+            generateButton,
+            showAllButton,
+            removeAllButton
+            ])
+        stackButtons.axis = .horizontal
+        stackButtons.distribution = .fillEqually
+        view.addSubview(stackButtons)
+        
+        collectionView.contentInset = UIEdgeInsets.all(20)
         collectionView.register(CollectionViewCell.self, forCellWithReuseIdentifier: CollectionViewCell.identifier)
         collectionView.delegate = self
         view.addSubview(collectionView)
-
-        generateButton.snp.makeConstraints { (make) -> Void in
-            make.centerX.equalToSuperview()
+        
+        stackButtons.snp.makeConstraints { (make) -> Void in
             make.top.equalTo(view.safeAreaLayoutGuide).offset(24)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(42)
         }
         collectionView.snp.makeConstraints { make in
             make.top.equalTo(generateButton.snp.bottom)
@@ -89,14 +115,15 @@ class ViewController: UIViewController {
 
 extension ViewController : UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: collectionView.frame.inset(by: collectionView.contentInset).width, height: 50)
+        return CGSize(width: collectionView.frame.inset(by: collectionView.contentInset).width, height: 70)
     }
 }
 
 extension UIButton {
-    class func image(_ image: UIImage) -> UIButton {
-        let button = UIButton(type: .system)
+    class func image(_ image: UIImage, selected: UIImage? = nil) -> UIButton {
+        let button = UIButton(type: .custom)
         button.setImage(image, for: .normal)
+        button.setImage(selected, for: .selected)
         button.imageView?.contentMode = .scaleAspectFit
         button.snp.makeConstraints { make in
             make.width.height.equalTo(24)
@@ -114,6 +141,7 @@ class CollectionViewCell: UICollectionViewCell {
     let plus = UIButton.image(#imageLiteral(resourceName: "plus"))
     let minus = UIButton.image(#imageLiteral(resourceName: "minus"))
     let delete = UIButton.image(#imageLiteral(resourceName: "delete"))
+    let check = UIButton.image(#imageLiteral(resourceName: "unchecked"), selected: #imageLiteral(resourceName: "checked"))
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -125,19 +153,34 @@ class CollectionViewCell: UICollectionViewCell {
     }
 
     func ui() {
-        let stackView = UIStackView(arrangedSubviews: [
-            label,
+        self.layer.borderColor = UIColor.darkGray.withAlphaComponent(0.3).cgColor
+        self.layer.borderWidth = 1
+        self.layer.cornerRadius = 2
+        self.layoutMargins = UIEdgeInsets.symmetric(vertical: 8, horizontal: 12)
+        
+        let stackButtons = UIStackView(arrangedSubviews: [
+            UIView(),
             plus,
             minus,
+            check,
             delete
             ])
-        stackView.axis = .horizontal
-        stackView.alignment = .center
+        stackButtons.axis = .horizontal
+        stackButtons.alignment = .center
+        stackButtons.distribution = .fill
+        stackButtons.spacing = 8
+        
+        let stackView = UIStackView(arrangedSubviews: [
+            label,
+            stackButtons
+            ])
+        stackView.axis = .vertical
+        stackView.alignment = .fill
         stackView.distribution = .fill
         stackView.spacing = 8
         contentView.addSubview(stackView)
         stackView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
+            make.edges.equalTo(self.snp.margins)
         }
     }
 
